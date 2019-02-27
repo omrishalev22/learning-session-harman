cf0d8adf4ee46313aa20b9e6fb0a80a420369c12
shalevo <omrimexico@gmail.com>
2019-02-27 08:41:16 +0200

    project sass support - 7 to 1 structure

--- learning session/harman-multi-container/client/src/App.js
+++ learning session/harman-multi-container/client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import SearchSection from './sections/SearchSection';
@@ -23,7 +22,7 @@ class App extends Component {
                             <Route exact path="/" component={AddSection}/>
                         </section>
                     </main>
-
+                    <footer className="footer"></footer>
                 </div>
             </Router>
         );
